9ebc1b49361dad74d257fbf32e3cf376a3975485
Pranav <pranavkumar192192@gmail.com>
2024-04-04 09:26:25 +0530

    updated navbar according to pathname

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import SigninButton from "./SigninButton";
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Appbar = () => {
+  const pathname = usePathname();
+
+  if (pathname === "/dashboard") {
+    return null;
+  }
+
   return (
-    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow ">
-      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href={"/"}>
+    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href="/">
         Home
       </Link>
-      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href={"/profile"}>
+      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href="/profile">
         User Profile
       </Link>
-      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href={"/dashboard"}>
+      <Link className="text-gray-800 hover:text-sky-400 transition-colors" href="/dashboard">
         Dashboard
       </Link>
-
-      <SigninButton />
+      {/* <SigninButton /> */}
     </header>
   );
 };
